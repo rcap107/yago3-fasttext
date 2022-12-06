e849f98c0d147218d158cfd0d1d80247b716a294
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2022-12-06 16:11:16 +0100

    Fixed formatting, added path checks.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import pyarrow.parquet as pq
 import os.path as osp
+
 import fasttext
-import os.path as osp
-import fasttext
-from joblib import Parallel, delayed
+import numpy as np
 import pandas as pd
+import pyarrow.parquet as pq
 from tqdm import tqdm
-import pickle
 
 FASTTEXT_MODEL_PATH = "~/store/cc.en.300.bin"
 
@@ -14,10 +12,7 @@ FASTTEXT_MODEL_PATH = "~/store/cc.en.300.bin"
 def extract_entities(path_to_embs, path_to_entities=None):
     # Reading pq file with KEN embeddings
     tab = pq.read_table(path_to_embs)
-    df = tab.to_pandas()
-    entities = df["Entity"]
-    # Cleaning entity names
-    entities = entities.str.replace("<", "").str.replace(">", "").str.replace("_", " ")
+    entities = tab.to_pandas()
 
     # Saving entities list to csv
     if path_to_entities is not None:
@@ -25,30 +20,52 @@ def extract_entities(path_to_embs, path_to_entities=None):
     return entities    
 
 def generate_embeddings(entities, output_emb_path):
+    model = load_model()
+    
+    assert type(entities) == pd.DataFrame
+    emb_array = populate_array(entities, model)
+ 
+    df = convert_array_to_df(entities, emb_array) 
+
+    print("Saving file to parquet.")
+    df.to_parquet(output_emb_path)
+
+def populate_array(entities, model):
+    print("Creating table")
+    emb_array = np.zeros((len(entities), 300))
+    for _, e in tqdm(entities.iterrows(), total=len(entities)):
+        e_key = e["Entity"]
+        entity_str = e_key.replace("<", "").replace(">", "").replace("_", " ")
+        entity_emb =  model.get_sentence_vector(entity_str)
+        emb_array[_, :] = entity_emb
+    return emb_array
+
+def convert_array_to_df(entities, emb_array):
+    print("Converting np.array to pd.DataFrame")
+    df = pd.DataFrame(emb_array, index=entities["Entity"])
+    df.columns = [f'X{_}' for _ in df.columns]  # type: ignore
+    return df
+    
+def load_model():
     model_path = osp.expanduser(FASTTEXT_MODEL_PATH)
     assert osp.exists(model_path)
-
-    model = fasttext.load_model(model_path)
     
-    emb_dict = {}
-    for _, e in tqdm(entities.iterrows(), total=len(entities)):
-        entity_str = e["Entity"].lower()
-        entity_emb =  model.get_sentence_vector(entity_str)
-        emb_dict[entity_str] = entity_emb
-
-    # This takes a very long time to run! 
-    print("Converting dict to pd.DataFrame")
-    df = pd.DataFrame().from_dict(emb_dict, orient="index")
-    df.columns = [f'X{_}' for _ in df.columns]  # type: ignore
-    print("Saving df to parquet file.")
-    df.reset_index().to_parquet(f"{output_emb_path}")
-
+    model = fasttext.load_model(model_path)
+    return model # type: ignore
+    
 
 if __name__ == "__main__":
     path_to_embs = osp.expanduser("~/store/emb_mure_ken_yago3_full.parquet")
     assert osp.exists(path_to_embs)
+
     path_to_entities = "data/yago_entities.csv"
-    entities = extract_entities(path_to_embs, path_to_entities)
+
+    if not osp.exists(path_to_entities):
+        print("Extracting entities.")
+        entities = extract_entities(path_to_embs, path_to_entities)
+    else:
+        print("Loading entities.")
+        entities = pd.read_csv(path_to_entities)
     output_emb_path = "data/yago3-fasttext.parquet"
     generate_embeddings(entities, output_emb_path)
 
